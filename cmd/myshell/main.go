45da89b493b50444f41f94fd9bfe6fd3a3bfd528
Naman Jain <naman.jain@msbdocs.com>
2024-06-17 22:21:12 +0530

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading the command: ", err)
+	}
+	command = strings.Trim(command, "\n")
+	fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 }
