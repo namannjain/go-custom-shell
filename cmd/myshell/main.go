8c2f194174d61cdb642ea244eede8292a6f37107
Naman Jain <naman.jain@msbdocs.com>
2024-06-17 22:49:51 +0530

    pass stage echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,10 +6,13 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	builtinCommands := []string{"echo", "exit", "cat", "type"}
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -17,14 +20,24 @@ func main() {
 		input = strings.TrimSpace(input)
 
 		commands := strings.Split(input, " ")
+
 		switch commands[0] {
+		case "type":
+			if slices.Contains(builtinCommands, commands[1]) {
+				fmt.Printf("%s is a shell builtin\n", commands[1])
+			} else {
+				fmt.Printf("%s: not found\n", commands[1])
+			}
+
 		case "exit":
 			exitCode, err := strconv.Atoi(commands[1])
 			if err == nil {
 				os.Exit(exitCode)
 			}
+
 		case "echo":
 			fmt.Fprintf(os.Stdout, "%s\n", strings.Join(commands[1:], " "))
+
 		default:
 			fmt.Printf("%s: command not found\n", input)
 		}
